b869eda1d21ab76dca66447bee8512bd16e8e854
Navamy <Navamy@Akhil-PC>
2024-11-30 15:13:49 +0530

    comiit

--- 7RMartProject/src/main/java/pages/ManageFooterPage.java
+++ 7RMartProject/src/main/java/pages/ManageFooterPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,6 +12,46 @@ public class ManageFooterPage {
 		this.driver=driver;
 		PageFactory.initElements(driver,this);
 	}
-	@FindBy(xpath="") private WebElement ;
+	@FindBy(css="a.small-box-footer[href='https://groceryapp.uniqassosiates.com/admin/list-footertext']") private WebElement moreinfobutton;
+	@FindBy(xpath="//a[@href='https://groceryapp.uniqassosiates.com/admin/Footertext/edit?edit=1']") private WebElement updateicon ;
+	@FindBy(xpath="//textarea[@name='address']") private WebElement addressfield;
+	@FindBy(xpath="//input[@id='email']") private WebElement emailfield;
+	@FindBy(xpath="//input[@id='phone']") private WebElement phonefield ;
+	@FindBy(xpath="//button[@type='submit']") private WebElement updatebutton;
+	@FindBy(xpath="//h5[text()=' Alert!']") private WebElement alertmessage;
+	
+	public void clickOnMoreinfobutton() {
+		//moreinfobutton.click();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();",moreinfobutton);
+	}
+	public void clickOnUpdateicon() {
+		updateicon.click();
+	}
+	public void enterAddress(String passingaddress) {
+		addressfield.clear();
+		addressfield.sendKeys(passingaddress);
+	}
+	public void enterEmail(String passingemail) {
+		emailfield.clear();
+		emailfield.sendKeys(passingemail);
+	}
+	public void enterPhonenumber(String passphonenumber) {
+		phonefield.clear();
+		phonefield.sendKeys(passphonenumber);
+	}
+	public void clickOnUpdatebutton() {
+		//updatebutton.click();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();",updatebutton);
+	}
+	public boolean isAlertMessageDisplayed() {
+		return alertmessage.isDisplayed();
+	}
+	public boolean isUpdateButtonDisplayed() {
+		return updatebutton.isDisplayed();
+	}
+	
+	
 	
 }
--- 7RMartProject/src/test/java/test/script/ManageFooterTest.java
+++ 7RMartProject/src/test/java/test/script/ManageFooterTest.java
@@ -2,9 +2,12 @@ package test.script;
 
 import java.io.IOException;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import pages.LoginPage;
+import pages.ManageContactPage;
+import pages.ManageFooterPage;
 import utilities.ExcelUtilities;
 
 public class ManageFooterTest extends Base{
@@ -16,6 +19,19 @@ public class ManageFooterTest extends Base{
 		loginpage.enterUsernameOnUsernameField(loginusernamevalue);
 		loginpage.enterPasswordOnPasswordField(loginpasswordvalue);
 		loginpage.clickOnSigninButton();
+		
+		String address=ExcelUtilities.getStringData(1, 0,"ManageFooterPage");
+		String email=ExcelUtilities.getStringData(1, 1,"ManageFooterPage");
+		String phone=ExcelUtilities.getIntegerData(1, 2,"ManageFooterPage");
+		ManageFooterPage managefooterpage=new ManageFooterPage(driver);
+		managefooterpage.clickOnMoreinfobutton();
+		managefooterpage.clickOnUpdateicon();
+		managefooterpage.enterAddress(address);
+		managefooterpage.enterEmail(email);
+		managefooterpage.enterPhonenumber(phone);
+		managefooterpage.clickOnUpdatebutton();
+		boolean alertdisplayed=managefooterpage.isAlertMessageDisplayed();
+		Assert.assertTrue(alertdisplayed,"Alert not displayed");
 	}
 	@Test
 	public void checkPresenceOfUpdatebuttonInManageFooter() throws IOException {
@@ -25,5 +41,11 @@ public class ManageFooterTest extends Base{
 		loginpage.enterUsernameOnUsernameField(loginusernamevalue);
 		loginpage.enterPasswordOnPasswordField(loginpasswordvalue);
 		loginpage.clickOnSigninButton();
+		
+		ManageFooterPage managefooterpage=new ManageFooterPage(driver);
+		managefooterpage.clickOnMoreinfobutton();
+		managefooterpage.clickOnUpdateicon();
+		boolean updatebuttondisplayed=managefooterpage.isUpdateButtonDisplayed();
+		Assert.assertTrue(updatebuttondisplayed,"Update Button not displayed");
 	}
 }
