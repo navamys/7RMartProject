9b24c9386fcfa299c6763fbad916572bf5bc85e1
Navamy <Navamy@Akhil-PC>
2024-12-06 10:06:02 +0530

    commit

--- 7RMartProject/src/main/java/pages/HomePage.java
+++ 7RMartProject/src/main/java/pages/HomePage.java
@@ -1,5 +1,28 @@
 package pages;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class HomePage {
+	WebDriver driver;
+	public HomePage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver,this);	
+	}
+	@FindBy(xpath="//a[@data-toggle='dropdown']")private WebElement adminimagebutton;
+	@FindBy(css="a.dropdown-item[href='https://groceryapp.uniqassosiates.com/admin/logout']")private WebElement logoutbutton;
+	@FindBy(xpath="//button[@type='submit']")private WebElement signinbutton;
+	
+	public void clickOnAdminimagebutton() {
+		adminimagebutton.click();
+	}
+	public void clickOnLogoutbutton() {
+		logoutbutton.click();
+	}
+	public boolean isSigninButtonDisplayed() {
+		return signinbutton.isDisplayed();
+	}
+	
 }
--- 7RMartProject/src/test/java/test/script/HomeTest.java
+++ 7RMartProject/src/test/java/test/script/HomeTest.java
@@ -1,5 +1,30 @@
 package test.script;
 
-public class HomeTest {
+import java.io.IOException;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.AdminuserPage;
+import pages.HomePage;
+import pages.LoginPage;
+import utilities.ExcelUtilities;
+
+public class HomeTest extends Base{
+	@Test
+	public void verifyIfUserIsAbleToLogout() throws IOException {
+		String usernamevalue=ExcelUtilities.getStringData(1, 0,"LoginPage"); 
+		String passwordvalue=ExcelUtilities.getStringData(1, 1,"LoginPage");
+		LoginPage loginpage=new LoginPage(driver);
+		loginpage.enterUsernameOnUsernameField(usernamevalue);
+		loginpage.enterPasswordOnPasswordField(passwordvalue);
+		loginpage.clickOnSigninButton();
+		
+		HomePage homepage=new HomePage(driver);
+		homepage.clickOnAdminimagebutton();
+		homepage.clickOnLogoutbutton();
+		boolean signinbuttondisplayed=homepage.isSigninButtonDisplayed();
+		Assert.assertTrue(signinbuttondisplayed,"signin button is not displayed as logout is not successful");
+
+	}
 }
